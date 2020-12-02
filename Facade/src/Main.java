f6233da3c9568a4fb1ab44ff8ff64e764e3f37dc
junkatou <jkky0225@gmail.com>
2020-12-03 07:34:51 +0900

    Facadeの実装

---
+++ Facade/src/Main.java
@@ -0,0 +1,10 @@
+import pagemaker.PageMaker;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		PageMaker.makeWelcomePage("hyuki@hyuki.com", "Welcome.html");
+	}
+
+}
